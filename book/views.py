fd319d0192baa9ef3e290546255c54f2640cac7a
nadyrbek97 <nadyrbek97@gmail.com>
2019-06-24 16:02:47 +0600

    add Similar Books functionality

--- book/views.py
+++ book/views.py
@@ -2,6 +2,13 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django import views
 from django.views.generic import DetailView
 from django.contrib import messages
+from django.db.models import Count
+"""
+        This is the Count aggregation function of the Django ORM. This
+    function will allow us to perform aggregated counts of tags.
+    django.db.models includes the following aggregation functions:
+"""
+
 
 from .models import (Book,
                      Category,
@@ -47,6 +54,28 @@ def book_detail(request, pk):
     # List of all comments
     comments = book.comments.all()
 
+    # List of similar books
+    book_tag_ids = book.tags.values_list('id', flat=True)
+    similar_books = Book.objects.filter(tags__in=book_tag_ids).exclude(id=book.id)
+    similar_books = similar_books.annotate(same_tags=Count('tags')).order_by('-same_tags')[:4]
+    """
+        1. We retrieve a Python list of IDs for the tags of the current
+        post. The values_list() QuerySet returns tuples with the
+        values for the given fields. We pass flat=True to it to get a flat
+        list like [1, 2, 3, ...] .
+        
+        2. We get all posts that contain any of these tags, excluding the
+        current post itself.
+        
+        3. We use the Count aggregation function to generate a
+        calculated field— same_tags —that contains the number of tagsshared with all the tags queried.
+        
+        4. We order the result by the number of shared tags
+        (descending order) and by publish to display recent posts first
+        for the posts with the same number of shared tags. We slice
+        the result to retrieve only the first four posts.
+    """
+
     if request.method == "POST":
         # means comment was posted
         comment_form = CommentForm(request.POST)
@@ -69,7 +98,8 @@ def book_detail(request, pk):
                   'book/book_detail.html',
                   context={'book': book,
                            'comments': comments,
-                           'comment_form': comment_form})
+                           'comment_form': comment_form,
+                           'similar_books': similar_books})
 
 
 def book_tag_list(request, tag_slug=None):
